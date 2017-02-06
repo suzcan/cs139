5fc216bb565a60925a1925a4fda72cfa488e1791
suzcan <suzypcg@gmail.com>
2017-02-06 07:32:27 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
-insert into users values("23", "password", "userTwo", "usert@user.com", "0");
-insert into listCat values("School", "so many lists", "0", "tom");
-insert into lists values("Materials To Buy", "so many", 
+INSERT INTO users VALUES("23", "password", "userTwo", "usert@user.com", "0");
+INSERT INTO listCats VALUES("School", "so many lists", "0", "userOne");
+INSERT INTO lists VALUES("Materials To Buy", "so many", 1);
+INSERT INTO users VALUES("2", "password", "userThree", "user@user.com", "1");
+INSERT INTO listCats VALUES("Work", "lots", "0", "userTwo");
