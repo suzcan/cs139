4ac38048a2124e2a498656a80b8562457831552b
suzcan <suzypcg@gmail.com>
2017-02-06 06:45:21 +0000

    Update schema.sql
    changed blob values into integer

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE users;
 CREATE TABLE users(userID integer, password varchar(20), username varchar(20), email text, notifPref integer);
 DROP TABLE listCats;
-CREATE TABLE listCats(name varchar(25), lists text, dNotd blob, sharedW text);
+CREATE TABLE listCats(name varchar(25), lists text, dNotd integer, sharedW text);
 DROP TABLE lists;
-CREATE TABLE lists(name varchar(25), listItems text, dNotd blob, sharedW text);
+CREATE TABLE lists(name varchar(25), listItems text, dNotd integer, sharedW text);
 DROP TABLE listItems;
-CREATE TABLE listItems(numeration integer, content text, dNotd blob);
+CREATE TABLE listItems(numeration integer, content text, dNotd integer);
