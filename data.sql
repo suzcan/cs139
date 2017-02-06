1a1d22f3d5ff0d4b9f49e5fab58514d8998911b9
suzcan <suzypcg@gmail.com>
2017-02-06 16:26:49 +0000

    Update data.sql
    added missing "

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@ INSERT INTO users VALUES("1", "password", "userOne", "user1@user.com", "1");
 INSERT INTO listCats VALUES("1", "Meetings to Attend", "1");
 INSERT INTO lists VALUES("1", "February Meetings", "1", "0", "userTwo");
 INSERT INTO listItems VALUES("1", "Brunch Meeting", "0", "1");
-INSERT INTO listItems VALUES("2", "Lunch Meeting, "0", "1");
+INSERT INTO listItems VALUES("2", "Lunch Meeting", "0", "1");
 INSERT INTO users VALUES("2", "password", "userTwo", "user2@user.com", "0");
 INSERT INTO listCats VALUES("2", "Things to Do", "2");
 INSERT INTO lists VALUES("2", "Chores for Monday", "2", "0", "no one");
