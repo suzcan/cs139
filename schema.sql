64b48666f47c0a513d7bba33d2d2789fa0590794
suzcan <suzypcg@gmail.com>
2017-02-06 06:39:32 +0000

    Update schema.sql
    added drop commands and capatilization for readability.

--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
-create table users(userID integer, password varchar(20), username varchar(20), email text, notifPref integer);
-create table listCat(name varchar(25), lists text, dNotd blob, sharedW text);
-create table lists(name varchar(25), listItems text, dNotd blob, sharedW text);
-create table listItems(numeration integer, content text, dNotd blob);
+DROP TABLE users;
+CREATE TABLE users(userID integer, password varchar(20), username varchar(20), email text, notifPref integer);
+DROP TABLE listCats;
+CREATE TABLE listCats(name varchar(25), lists text, dNotd blob, sharedW text);
+DROP TABLE lists;
+CREATE TABLE lists(name varchar(25), listItems text, dNotd blob, sharedW text);
+DROP TABLE listItems;
+CREATE TABLE listItems(numeration integer, content text, dNotd blob);
